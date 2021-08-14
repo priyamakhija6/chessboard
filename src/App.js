84b46a5344d228852ceba7877952eb50e4c34b1b
Priya Makhija <priya.makhija24@gmail.com>
2021-08-13 21:28:34 -0400

    all at once

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import './style.css';
 
 function App() {
+    const findValidMove = (row,col) => {
+        const N = 8;
+        let dx = [2,2,-2,-2,1,1,-1,-1];
+        let dy = [2,2,-2,-2,1,1,-1,-1];
+        const board = new Array(N).fill(" ");
+
+        if((row>=0 && row<=7) && (col>=0 && col<=7) && board[row][col]== " ") {
+            board[row][col] = 8
+        }
+
+    }
   return (
+    <>
+    <h1>Chessboard</h1>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChessBoard />
     </div>
+    </>
   );
+}
+
+const ChessBoard = () => {
+    const N = 8;
+    const arr = new Array(N).fill(0);
+    return (
+        <table>
+            <tbody className="chess">
+            {
+                arr.map((val,idx)=> {
+                    return (
+                        <ChessBoardRow key={"g"+idx} row={idx} findValidMove={findValidMove}/>
+                    )
+                })
+            }
+            </tbody>
+        </table>
+    )
+}
+
+const ChessBoardRow = (props) => {
+    const squareClicked = (i, j) => {
+        console.log(i, j);
+        props.findValidMove(i,j)
+    }
+
+    const rowNum = props.row;
+    const arr = new Array(8).fill(0);
+    return (
+        <tr className="row">
+            {
+                arr.map((column,idx) => {
+                    return (
+                        <td className="square" key={idx} onClick={squareClicked.bind(this, rowNum, idx)}></td>
+                    )
+                })
+            }
+        </tr>
+    )
 }
 
 export default App;
